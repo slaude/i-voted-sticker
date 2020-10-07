55c7a09d84cf66cde364844aeee639216e8d03fd
Sean Laude <sean.laude@braze.com>
2020-10-07 11:20:06 +0200

    MVP

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# i-voted-sticker
+# "I voted" sticker generator
+
+## Impetus
+
+I moved abroad before the 2020 election, and before that, voted by mail in the New York State primary while on lockdown. Unfortunately, not all absentee ballots come with an "I voted" sticker that are a regular part of U.S. elections.
+
+Overseas voters are given resources to request a ballot from the [Foreign Voter Assistance Program](https://www.fvap.gov/) - as well as a set of stickers, but only for a [pre-defined list of countries](https://www.fvap.gov/IVotedSticker?id=219). I figured I could hack this with a few lines of code in developer tools, but it turns out they're just [.jpgs](https://www.fvap.gov/uploads/FVAP/I-Voted-Sticker/Svalbard%20and%20Jan%20Mayen.jpg) that someone had to recreate and upload for 254 different countries and subnational entities (as of the last commit to this README). This is my recreation of the sticker, but for *any* arbitrary location.
+
+So, if you're dying for a sticker because your municipality does not send them with your ballot or really want to say you voted from your studio apartment in Brooklyn, you've come to the right place!
+
+## Things that are worth adding before the next election cycle
+
+- Support for arbitrary languages
+- `@media print` styling
+- embiggen the text instead of monotonically ensmalliing it on text change
+- jazzy graphics and geometries
+
+## Technical notes
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/App.js
@@ -0,0 +1,156 @@
+import React, { useState, useRef, useEffect } from 'react';
+import './App.css';
+
+const parseURL = () => {
+  return window.document.location.search
+    .substr(1)
+    .split("&")
+    .map((part) => {
+      const [key, encoded] = part.split('=')
+      return {
+        key,
+        value: decodeURI(encoded),
+      };
+    })
+    .reduce((acc, val) => {
+      acc[val.key] = val.value
+      return acc;
+    }, {});
+}
+
+const DEFAULT_SIZES_REMS = [8, 6, 4.5, 3.33, 3, 2.5, 2, 1.75, 1.5, 1, 0.75];
+
+export default () => {
+  const [askIfYouVoted, setHasAsked] = useState(false);
+  const [hasVoted, setHasVoted] = useState(false);
+
+  const [location, setLocation] = useState(parseURL()?.location || "");
+  const [locationSize, setLocationSize] = useState(DEFAULT_SIZES_REMS[0]);
+  const lastSize = useRef(null);
+
+  const locationRef = useRef(null);
+  const locationWrapperRef = useRef(null);
+
+  useEffect(() => {
+    window.history.replaceState(null, "", `?location=${encodeURI(location)}`);
+  }, [location]);
+
+  useEffect(() => {
+    // force the recalc of the text size on mount if location is already populated
+    if(locationRef.current) {
+      // this is super hacky, but append a space to `location`
+      // to get the effect below to run
+      setLocation( l => l ? l+" " : l);
+    }
+  }, [askIfYouVoted, locationRef, setLocation]);
+
+  useEffect(() => {
+    if(locationRef.current === null || locationWrapperRef.current === null) {
+      return;
+    }
+
+    const height = locationRef.current.clientHeight;
+    const parentHeight = locationWrapperRef.current.clientHeight;
+
+    if(!location) {
+      // if the text gets blanked, reset to the largest in the scale
+      setLocationSize(DEFAULT_SIZES_REMS[0])
+    } else if(height > parentHeight) {
+      // attempt to decrease the size to the next smallest size in the scale
+      const nextSize = DEFAULT_SIZES_REMS[DEFAULT_SIZES_REMS.indexOf(locationSize) + 1];
+      
+      if(nextSize) {
+        lastSize.current = locationSize;
+        setLocationSize(nextSize);
+      }
+    }
+
+  }, [location, setLocationSize, locationSize, locationRef, locationWrapperRef])
+
+  return (
+    <div className="App">
+      <h1>Create your own "I voted" sticker</h1>
+      {askIfYouVoted ? (
+        <>
+          <div className="ElectoralismAsATreat">
+            {hasVoted ? 
+              <h2>
+                Great! Customize your sticker:
+              </h2> : 
+              <div>
+                Here's a <a href="https://www.usvotefoundation.org/">resource</a> to help you register or find your polling place.
+                We'll let you try making a sticker in the meantime:
+              </div>
+            }
+          </div>
+          <form onSubmit={(e) => { e.preventDefault() }}>
+            <fieldset>
+              <div>
+                <label htmlFor="location"><h3>Where are you currently?</h3></label>
+                <input type="text" value={location} onChange={e => setLocation(e.target.value)} id="location" placeholder="your location" />
+                <button type="button" className="ResetButton" onClick={() => setLocation("")}>
+                  Reset
+                </button>
+              </div>
+            </fieldset>
+          </form>
+          <div className="StickerWrapper">
+            <div className="BigOval">
+              <div className="StarsAndStripes">
+                <div className="Stars">
+                  <div className="Star">
+                    <div className="Star-inner White"></div>
+                  </div>
+                  <div className="Star">
+                    <div className="Star-inner White"></div>
+                  </div>
+                  <div className="Star">
+                    <div className="Star-inner White"></div>
+                  </div>
+                </div>
+                <div className="Stripes">
+                  <div className="Stripe Red"></div>
+                  <div className="Stripe"></div>
+                  <div className="Stripe Red"></div>
+                </div>
+              </div>
+              <div className="IVotedPreposition">I Voted From</div>
+              <div className="LocationWrapper" ref={locationWrapperRef}>
+                <div
+                  className="Location"
+                  style={{
+                    fontSize: `${locationSize}rem`,
+                    lineHeight: `${locationSize}rem`
+                  }}
+                  ref={locationRef}
+                >
+                  {location}
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <h2>But first: have you voted yet?</h2>
+          <div className="ElectoralismAsATreat">
+            <button type="button" className="VoteButton" onClick={() => {
+              setHasAsked(true);
+              setHasVoted(true);
+            }}>
+              Yes!
+            </button>
+          </div>
+          <div className="ElectoralismAsATreat">
+            <button type="button" className="VoteButton" onClick={() => {
+              setHasAsked(true);
+              setHasVoted(false);
+            }}>
+              Not Yet!
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  )
+}
