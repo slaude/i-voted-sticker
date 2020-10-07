23a27415bf7c842681b8d0f97bd23bad0079f9fd
Sean Laude <sean.laude@braze.com>
2020-10-07 11:40:51 +0200

    Updates

---
+++ precache-manifest.837a2350db4c1114d4ff67288ca0fab8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3a63745cc7dcc61b85d0817dd48e9af",
+    "url": "/i-voted-sticker/index.html"
+  },
+  {
+    "revision": "ed14ef2deb2c6878dd65",
+    "url": "/i-voted-sticker/static/css/main.7232915e.chunk.css"
+  },
+  {
+    "revision": "41df12cf86fbad451417",
+    "url": "/i-voted-sticker/static/js/2.fd0927a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/i-voted-sticker/static/js/2.fd0927a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed14ef2deb2c6878dd65",
+    "url": "/i-voted-sticker/static/js/main.a163c17d.chunk.js"
+  },
+  {
+    "revision": "3c99f1bbe437f23d638e",
+    "url": "/i-voted-sticker/static/js/runtime-main.93c519ef.js"
+  }
+]);
